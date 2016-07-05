81a082a10a3b8fb91632f3008c30124f0b1e32ec
Vysakh P Pillai <vysakhpillai@gmail.com>
2016-07-05 14:27:15 +0530

    fixing paho script name

--- server/html/freeboardMQTT/plugins/mqtt/paho.mqtt.plugin.js
+++ server/html/freeboardMQTT/plugins/mqtt/paho.mqtt.plugin.js
@@ -10,7 +10,7 @@
 		"display_name": "Paho MQTT",
         "description" : "Receive data from an MQTT server.",
 		"external_scripts" : [
-        "plugins/mqtt/mqttws31.min.js",
+        "plugins/mqtt/mqttws31.js",
 		],
 		"settings"    : [
 			{
